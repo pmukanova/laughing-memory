1fa604126c63c64496eabab53dcce6f911e48b47
Perizat Menard <perizat.m@haystacks.ai>
2022-03-25 11:03:46 -0400

    working with point geometry

---
+++ creating_point.py
@@ -0,0 +1,8 @@
+# Import the Point geometry
+from shapely.geometry import Point
+
+# Construct a point object for the Eiffel Tower
+eiffel_tower = Point(255422.6,6250868.9)
+
+# Print the result
+print(eiffel_tower)
